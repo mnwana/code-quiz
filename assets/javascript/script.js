aaf59b293dd29d5af43277d80a2e1012cc227f7b
mnwana <mariellenwana@gmail.com>
2022-07-03 19:06:57 -0400

    add some pseudocode

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,12 @@
 // initialize starting values for question array, quiz timer and main/header
 var questions = [];
+var questionNum = 0;
 var quizTimer = 60;
 var answersEl = document.querySelector("#answers");
-var startQuizEl = document.querySelector("#start-quiz")
+var startQuizEl = document.querySelector("#start-quiz");
+var timerEl = document.querySelector("#timer");
 
-// load question text 
+// load question text
 // TODO: will look to change this to read from file or something
 var loadQuestion = function (
   questionText,
@@ -25,12 +27,37 @@ var loadQuestion = function (
   questions.push(question);
 };
 
+var answersHandler = function (event) {
+  console.dir(event);
+};
+
+var startQuizHandler = function (event) {
+  window.alert("Quiz is starting");
+// cycle through questions
+    // start timer countdown
+    displayQuiz(questions[questionNum]);
+};
+
+var displayQuiz = function(questionNum){
+    // display question
+
+    // when option clicked
+
+        // if !isCorrect, deduct 10 seconds from timer & display "Wrong!"
+        // if isCorrect, display "Correct!"
+    questionNum++;
+    // if there is another question, display next question 
+    // else get high score input
+}
+// sort questions in random order 
+var sortQuestions = function (){
+
+}
+// load 2 questions to test with & shuffle
 loadQuestion("What is my name", "Marielle", "Mariellen", "Marie", "Mary");
 loadQuestion("What is his name", "William", "Willy", "Will", "Walt");
-
-var answersHandler = function(event){
-    console.dir(event);
-}
-
-answersEl.addEventListener("click", answersHandler);
+sortQuestions();
+// initialize timer
+timerEl.textContent = "Time: " + quizTimer;
 startQuizEl.addEventListener("click", startQuizHandler);
+answers.addEventListener("click",displayQuiz)
